6a5d0aac8dd2eb2c3a3ebd3fe16d68ecca4bb35c
ImadQADDOURI <imad.qaddouri@ump.ac.ma>
2023-04-29 09:18:46 +0100

    cooool

---
+++ pages/api/upload.js
@@ -0,0 +1,23 @@
+import multiparty from 'multiparty';
+import { resolve } from 'styled-jsx/css';
+
+
+export default async function handle(req,res){
+
+    const form = new multiparty.Form();
+    const {fields,files} = await new Promise( (resolve, reject) => {
+
+        form.parse(req , (err,fields,files) => {
+            if(err) reject(err);
+            resolve({fields,files});
+        });
+
+    });
+
+    console.log(files);
+
+
+    res.json(':) uploaded');
+}  
+
+export const config = { api : {bodyParser:false},};
