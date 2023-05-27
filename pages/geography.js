4d2fb86184197e45f6185fb01aeb71aeba210f65
ImadQADDOURI <imad.qaddouri@ump.ac.ma>
2023-05-27 18:01:57 +0100

    Geography zoom

--- pages/geography.js
+++ pages/geography.js
@@ -2,26 +2,47 @@ import Layout from "@/components/Layout";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
-
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, Graticule, Sphere } from "react-simple-maps";
 const geoUrl ="https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
 
-export default function OrdersPage() {
-  const [orders,setOrders] = useState([]);
+import { ZoomableGroup } from "react-simple-maps"
+
+
+export default function geo() {
+
   useEffect(() => {
   }, []);
+
+
+  const [position,setposition]=useState({coordinates:[0,0], zoom : 1});
+
+  const hundelMoveEnd = (position) => {
+    setposition(position);
+  }
+
   return (
     <Layout>
       <div className='hidden md:block w-full md:col-span-2 relative h-full  border rounded-lg bg-white ml-2 mt-2 '>
       <ComposableMap className=' m-4 p-2 h-fit'>
-        <Geographies geography={geoUrl}>
+      <ZoomableGroup
+      zoom={position.zoom}
+      center={position.coordinates}
+      onMoveEnd={hundelMoveEnd}
+      >
+        <Sphere  stroke="#808080" strokeWidth={0.2}/>
+        <Graticule stroke="#808080" strokeWidth={0.2} />
+          <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
               <Geography key={geo.rsmKey} geography={geo} />
             ))
           }
+
         </Geographies>
+
+      </ZoomableGroup>
+        
       </ComposableMap>
     </div>
       
